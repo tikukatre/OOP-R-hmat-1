9c0eb9db2e869d13d31ba2363445b8715b819f06
Katre <tikukatre@gmail.com>
2020-04-30 15:44:51 +0300

    Grafikaga katsetamist

--- Rühmatöö 2/src/main/java/Peaklass.java
+++ Rühmatöö 2/src/main/java/Peaklass.java
@@ -1,4 +1,8 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Peaklass extends Application {
@@ -15,6 +19,7 @@ public class Peaklass extends Application {
 
     public static void main(String[] args) {
         launch(args);
+
 
         //Kood vanast projektist, mis tuleb nüüd javafx graafikaga ümber teha
         /*
@@ -47,7 +52,61 @@ public class Peaklass extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        primaryStage.show();
+    public void start(Stage stage) {
+        Scene scene = new Scene(createBorderPane(), 250, 200);
+        stage.setTitle("Layout Demo");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public BorderPane createBorderPane() {
+
+        BorderPane borderPane = new BorderPane();
+/*
+        MenuBar menuBar = new MenuBar();
+        Menu peaMenyy = new Menu("Logitud andmed");
+        peaMenyy.getItems().addAll(new MenuItem("Salvesta uued andmed"),
+                new SeparatorMenuItem(), new MenuItem("Vaata logituid andmeid"));
+
+        Menu abiMenyy = new Menu("Abi");
+        abiMenyy.getItems().addAll(new MenuItem("Kuidas andmeid sisestada?"),
+                new SeparatorMenuItem(), new MenuItem("Rohkem infot programmist."));
+        menuBar.getMenus().addAll(peaMenyy, abiMenyy);
+
+
+ */
+        ToolBar toolbar = new ToolBar(new Separator(),
+                new Button("Vaata andmeid"), new Separator(), new Button("Salvesta andmed"),new Separator(),
+                new Button("Muuda andmeid"));
+
+        VBox vbox = new VBox();
+        vbox.getChildren().addAll( toolbar);
+
+        /*TreeItem<String> ti = new TreeItem<>("Projects");
+        ti.getChildren().addAll(
+                new TreeItem<String>("Project 1"),
+                new TreeItem<String>("Project 2"),
+                new TreeItem<String>("Project 3"),
+                new TreeItem<String>("Project 4"));
+        TreeView<String> tv = new TreeView<String>(ti);
+
+         */
+
+        TabPane tabPaneLeft = new TabPane();
+        Tab tab1 = new Tab("Project List");
+        //tab1.setContent(tv);
+        tabPaneLeft.getTabs().addAll(tab1, new Tab("Explorer"));
+
+        TabPane tabPaneRight = new TabPane();
+        tabPaneRight.getTabs().addAll(new Tab("Outline"),
+                new Tab("Task List"));
+
+        borderPane.setTop(vbox);
+        borderPane.setLeft(tabPaneLeft);
+        borderPane.setCenter(new TextArea());
+        borderPane.setRight(tabPaneRight);
+        borderPane.setBottom(new Label("Status text: Borderpane demo"));
+
+        return borderPane;
     }
 }
